fbf84973ab087cba68929107f6d862412161684f
Adnan Alam <30425077+AlwaysUP@users.noreply.github.com>
2019-02-09 19:50:37 -0500

    Update core.php

--- config/core.php
+++ config/core.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
  
 // home page url
 // $home_url="http://localhost/api/";
-$home_url="https://routine.buzztrench.com/api/";
+$home_url="localhost";
  
 // page given in URL parameter, default page is one
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
