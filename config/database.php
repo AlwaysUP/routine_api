2582eaf9db8d7bb1683f58a5586953b2e04390e4
Adnan Alam <30425077+AlwaysUP@users.noreply.github.com>
2019-02-09 19:50:59 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -4,8 +4,8 @@ class Database{
     // specify your own database credentials
     private $host = "localhost";
     private $db_name = "workout";
-    private $username = "root";
-    private $password = "";
+    private $username = "user";
+    private $password = "pass";
     public $connection;
  
     // get the database connection
